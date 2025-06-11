72adda3488ec2dac6d9b379e03e18cd98c7dc6bc
Marco Minerva <marco.minerva@gmail.com>
2025-06-11 11:47:38 +0200

    Add project files.

---
+++ Required500/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
+
+app.MapOpenApi();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", app.Environment.ApplicationName);
+});
+
+app.MapPost("/api/todo", (Todo todo) =>
+{
+    return TypedResults.Ok();
+})
+.WithSummary("Create a new todo item")
+.WithDescription("If we don't specify a value for the title property, we get a 500 error. If we comment out the 'app.UseExceptionHandler()' line, we'll get a 400 error instead.");
+
+app.Run();
+
+public class Todo
+{
+    public int Id { get; set; }
+
+    public required string Title { get; set; }
+}
